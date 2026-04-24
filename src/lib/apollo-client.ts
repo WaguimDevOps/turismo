553a2c7e2974fea5f0d2f5cac313f5fc34754f0c
WaguimDevOps <devwagnercrwa@gmail.com>
2026-04-24 17:26:59 -0300

    atualizei o apollo client

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -3,6 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 const client = new ApolloClient({
   link: new HttpLink({
     uri: process.env.NEXT_PUBLIC_WORDPRESS_API_URL || "http://turismo.local/graphql",
+    credentials: "include",
   }),
   cache: new InMemoryCache(),
 });
