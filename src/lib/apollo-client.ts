8b043e816c66bdca4804bca4982d94cbf64dd987
WaguimDevOps <devwagnercrwa@gmail.com>
2026-04-27 03:28:28 -0300

    Update Apollo Client to use GraphQL auth token

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -3,7 +3,9 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 const client = new ApolloClient({
   link: new HttpLink({
     uri: process.env.NEXT_PUBLIC_WORDPRESS_API_URL || "http://turismo.local/graphql",
-    credentials: "include",
+    headers: {
+      "X-WP-GraphQL-Token": process.env.NEXT_PUBLIC_GRAPHQL_TOKEN || "",
+    },
   }),
   cache: new InMemoryCache(),
 });
